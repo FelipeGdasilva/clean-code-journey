dcfc434ed8f2fc3d14ff3598ca6cc4d97fd00d93
FelipeGdasilva <felipegomes35@yahoo.com.br>
2026-05-26 11:09:17 -0300

    feat: adicionar exemplo de código sujo e limpo para nomes significativos

---
+++ 01-nomes-significativos/03-evite-codificacoes.js
@@ -0,0 +1,23 @@
+// O Código Sujo (Com codificação desnecessária):
+
+// Imagine que isso está no sistema do Sonic. 
+// O programador antigo colocou "s" para String, "i" para Inteiro e "b" para Booleano.
+const sNomePersonagem = "Sonic";
+const iVelocidadeMaxima = 100;
+const bPossuiEsmeraldaCaos = true;
+
+function fCalcular(oPersonagem, iDanoBase) {
+  // "o" para Objeto, "i" para Inteiro
+  return oPersonagem.iVelocidadeMaxima + iDanoBase;
+}
+
+//O Código Limpo (Sem codificações/prefixos):
+//O livro diz: confie no seu código e na sua ferramenta. Deixe os nomes limpos e pronunciáveis.
+
+const nomePersonagem = "Sonic";
+const velocidadeMaxima = 100;
+const possuiEsmeraldaDoCaos = true;
+
+function calcularDanoTotal(personagem, danoBase) {
+  return personagem.velocidadeMaxima + danoBase;
+}
